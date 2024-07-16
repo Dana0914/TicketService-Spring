cdf833f27beee8ba18fa6a471c0dcadfd3180184
Dana <Dana.ackerman0914@gmail.com>
2024-07-16 22:41:40 +0500

    Initial commit

--- src/main/java/kz/runtime/ticketservicespring/customexception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package kz.runtime.ticketservicespring.customexception;
-
-public class UserNotFoundException extends Exception {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
