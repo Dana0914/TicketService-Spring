c89073d835759c0a5f22c482c330578c03ee4272
Dana <Dana.ackerman0914@gmail.com>
2024-07-16 11:33:42 +0500

    Added getResource method

--- src/main/java/kz/runtime/ticketservicespring/main/Main.java
+++ src/main/java/kz/runtime/ticketservicespring/main/Main.java
@@ -7,13 +7,15 @@ import kz.runtime.ticketservicespring.entities.Ticket;
 import kz.runtime.ticketservicespring.entities.User;
 import kz.runtime.ticketservicespring.entities.dao.TicketDAO;
 import kz.runtime.ticketservicespring.entities.dao.UserDAO;
-import kz.runtime.ticketservicespring.entities.dao.UserService;
+import kz.runtime.ticketservicespring.serviice.UserService;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.Resource;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +28,12 @@ public class Main {
         config.addAnnotatedClass(Ticket.class).addAnnotatedClass(User.class);
         SessionFactory sessionFactory = context.getBean(SessionFactory.class);
 
+
+        Resource resource = context.getResource("file.txt");
+        List<String> resourceList = new ArrayList<>();
+        resourceList.add(getResource(resource));
+        System.out.println(resourceList);
+
         Ticket ticket = new Ticket();
         TicketDAO ticketDao = new TicketDAO(sessionFactory);
         User user = new User();
@@ -36,22 +44,14 @@ public class Main {
 
         user.setId(1L);
         user.setUsername("Olly");
-       user.setCreationDate(LocalDate.now());
+        user.setCreationDate(LocalDate.now());
 
 
         UserService userService = new UserService(ticketDao, userDao, true);
         userService.updateUserAndCreateTicket(ticket, user);
-
-
-        File file = context.getBean(File.class);
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        String input;
-        List<File> files = new ArrayList<>();
-        while ((input = reader.readLine()) != null) {
-            files.add(new File(input));
-        }
-        System.out.println(files);
-        sessionFactory.close();
-        reader.close();
     }
+    public static String getResource(Resource resource) throws IOException {
+        return new String(Files.readAllBytes(resource.getFile().toPath()));
+    }
+
 }
