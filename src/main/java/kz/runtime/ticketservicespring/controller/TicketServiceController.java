6d51943f2dd14cbf6f4798dadd0e3e3ba8b4a438
Dana <Dana.ackerman0914@gmail.com>
2024-07-18 14:11:56 +0500

    Added Spring Security support

--- src/main/java/kz/runtime/ticketservicespring/controller/TicketServiceController.java
+++ src/main/java/kz/runtime/ticketservicespring/controller/TicketServiceController.java
@@ -3,6 +3,7 @@ package kz.runtime.ticketservicespring.controller;
 
 import kz.runtime.ticketservicespring.service.TicketService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,11 @@ public class TicketServiceController {
     @GetMapping(value = "/get_ticket")
     public Object getTicket(@RequestParam(name = "id") Long id) {
         return ticketService.findTicketById(id);
+    }
+    @GetMapping(value = "/greeting")
+    public Object Greeting(Authentication authentication) {
+        return "Hello " + authentication.getName();
+
     }
 
 }
---
+++ src/main/java/kz/runtime/ticketservicespring/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package kz.runtime.ticketservicespring.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder().
+                username("admin").
+                password(passwordEncoder().encode("1234")).
+                roles("ADMIN").
+                build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+        return security.csrf(csrf -> csrf.disable()).authorizeHttpRequests(authorize ->
+                authorize.anyRequest().authenticated()).httpBasic(Customizer.withDefaults()).build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
